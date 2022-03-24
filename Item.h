bc797a99c456970136ee0ec071c70c83c13645a1
Sayuri <99312315+IT21238758@users.noreply.github.com>
2022-03-24 16:26:41 +0000

    Item.h completed

--- Item.h
+++ Item.h
@@ -1,17 +1,25 @@
 // Item.h
 
-class Item {
+class Item 
+{
    private:
-      int itemCode;
-      float unitPrice;
-      float discount; // out of 100 e.g. discount = 15
-   public:
-      // 1. Write the Default Constructor
-      // 2. Write the Overloaded Constructor
-      // 3. Write the Destructor
+    int itemCode;
+    float unitPrice;
+    float discount; // out of 100 e.g. discount = 15
+  
+public:
+  
+// 1. Write the Default Constructor
+    Item();
 
-      void setDiscount(float punitPrice);
-      float getDiscount();
-      float discountedPrice();
-      void display();
+// 2. Write the Overloaded Constructor
+    Item(int iCode , float uPrice);
+
+// 3. Write the Destructor
+    ~Item();
+
+    void setDiscount(float punitPrice);
+    float getDiscount();
+    float discountedPrice();
+    void display();
 };
