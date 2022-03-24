7355b9759b1bb6a4e23266a8520d0abf50a4ae89
Sayuri <99312315+IT21238758@users.noreply.github.com>
2022-03-24 16:21:38 +0000

    Exercise01.cpp completed

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+
 #include "Item.h"
+
 using namespace std;
 
 // DO NOT CHANGE THIS CODE
 
 int main() {
-    Item myItem;
+     Item myItem;
     myItem.display();
-    Item myItem2(100, 1000);
+  
+  Item myItem2(100 , 1000);
     myItem2.setDiscount(20);
     myItem2.display();
-    
+
     return 0;
 }
 
 // DO NOT CHANGE ABOVE CODE
-
